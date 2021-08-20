6958b6e00929347de6e2daaa6e16496fcae9648e
Preeti Maurya <47210718+ipreetimaurya@users.noreply.github.com>
2021-08-20 23:15:42 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h3> this is Footer </h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h3> this is Header </h3>
+    </div>
+  )
+}
+
+export default Header
